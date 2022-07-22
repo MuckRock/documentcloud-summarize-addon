245cad97420138bb34f390f194dc651fdc70d254
Jim Kang <jimkang@gmail.com>
2022-07-22 16:02:04 -0400

    Moved code from jimkang/documentcloud-summarize-addon.

--- main.py
+++ main.py
@@ -8,34 +8,77 @@ DocumentCloud using the standard API
 """
 
 from documentcloud.addon import AddOn
+import os
+from dotenv import load_dotenv
+import heapq
 
+load_dotenv()
+print('NLTK_DATA', os.environ['NLTK_DATA'])
+# .env needs to be loaded before importing nltk so that it knows
+# where to look for data.
+import nltk
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+stopwords = nltk.corpus.stopwords.words('english')
+
+# https://stackabuse.com/text-summarization-with-nltk-in-python/
+def summarize(doc):
+  sentence_list = nltk.sent_tokenize(doc.full_text)
+
+  word_frequencies = {}
+  for word in nltk.word_tokenize(doc.full_text):
+    if word not in stopwords:
+      if word not in word_frequencies.keys():
+        word_frequencies[word] = 1
+      else:
+        word_frequencies[word] += 1
+
+  maximum_frequency = max(word_frequencies.values())
+
+  for word in word_frequencies.keys():
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+  sentence_scores = {}
+  for sent in sentence_list:
+    for word in nltk.word_tokenize(sent.lower()):
+      if word in word_frequencies.keys():
+        if len(sent.split(' ')) < 30:
+          if sent not in sentence_scores.keys():
+            sentence_scores[sent] = word_frequencies[word]
+          else:
+            sentence_scores[sent] += word_frequencies[word]
+
+  summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+  summary = ' '.join(summary_sentences)
+
+  return summary
+
+class Summarize(AddOn):
+    """A document summarization Add-On for DocumentCloud."""
+
+    def add_summary(self, document, file_):
+        summary = summarize(document)
+        file_.write(f"{document.title}\n{document.canonical_url}\n\n{summary}\n\n")
+        self.set_message(f"Summarized {document.canonical_url}.")
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
+        self.set_message("Starting summarization.")
 
-        self.set_message("Hello World start!")
+        doc_list = self.client.documents.list(id__in=self.documents)
+        if not doc_list:
+            if self.query:
+                doc_list = self.client.documents.search(self.query)[:3]
+            else:
+                raise Exception("No documents found to summarize.")
 
-        # add a hello note to the first page of each selected document
-        if self.documents:
-            for document in self.client.documents.list(id__in=self.documents):
-                document.annotations.create(f"Hello {name}!", 0)
-        elif self.query:
-            documents = self.client.documents.search(self.query)[:3]
-            for document in documents:
-                document.annotations.create(f"Hello {name}!", 0)
+        with open("summaries.txt", "w+") as file_:
+          for document in doc_list:
+              self.add_summary(document, file_)
+          self.upload_file(file_)
 
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+        self.set_message("Summarization complete.")
+        self.send_mail(
+          "Summarize", f"We summarized: {', '.join([doc.title for doc in doc_list])}")
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    Summarize().main()
