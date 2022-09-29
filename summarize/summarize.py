a49e2ba053eb51fa7a438b73f9946f20e0dfe56a
Jim Kang <jimkang@gmail.com>
2022-09-29 12:29:09 -0400

    Add valid-word and sentence-length filtering for input and output, respectively.

--- summarize/summarize.py
+++ summarize/summarize.py
@@ -2,10 +2,12 @@ import pdb
 import math
 import numpy as np
 from annoy import AnnoyIndex
+import spacy
+import re
+
 from dotenv import load_dotenv
 load_dotenv()
 import nltk
-import spacy
 from nltk.cluster import KMeansClusterer, cosine_distance
 
 nlp = spacy.load('en_use_lg')
@@ -13,25 +15,28 @@ nlp = spacy.load('en_use_lg')
 #nlp.add_pipe('universal_sentence_encoder', config={'enable_cache': False})
 nlp.replace_pipe('universal_sentence_encoder', 'universal_sentence_encoder', config={'enable_cache': False})
 
-sentence_to_cluster_ratio = 40
-max_clusters = 5
-min_clusters = 1
+# TODO: Use \w minus digits to cover non-English languages.
+real_word_regex = re.compile(r'[a-zA-Z]+')
+# TODO: Parameterize
+sentence_to_cluster_ratio = 30
+max_clusters = 7
+min_clusters = 3
 sentences_to_pick_per_cluster = 1
 
 # Can GitHub Actions actually run tensorflow stuff?
 
 # TODO: Cache summaries.
-def summarize(text, max_sent_length = 30):
+def summarize(text, max_sent_words = 50, min_sent_words = 3):
   sentence_list = nltk.sent_tokenize(text)
+  sentence_list = [sent for sent in sentence_list if real_word_regex.search(sent)]
   if len(sentence_list) < 1:
     return # TODO: Raise error.
 
-  #sentence_embeddings = nlp(text)
-  #sentence_embeddings = nlp("Given the early stage of the investigations, the report will limit itself to a presentation of the main raw data and some first preliminary conclusions and interpretations. It is backed by two reports on the finds from the cistern (see elsewhere in this volume), in line with the project’s strategy to have a balanced input of effort and staff in the excavation and finds processing laboratory.")
   sentence_embeddings = [ get_vector(sentence) for sentence in sentence_list]
   if len(sentence_embeddings) < 1:
     return # TODO: Raise error.
   #print(sentence_embeddings)
+
   n_clusters = math.floor(len(sentence_embeddings)/sentence_to_cluster_ratio)
   if n_clusters < min_clusters:
     n_clusters = min_clusters
@@ -53,16 +58,22 @@ def summarize(text, max_sent_length = 30):
   t.build(10)
 
   nearest_to_centroids = [
-    t.get_nns_by_vector(centroid, sentences_to_pick_per_cluster)
+    t.get_nns_by_vector(centroid, sentences_to_pick_per_cluster + 4)
     for centroid in centroids
   ]
   nearest_to_centroids.sort()
   print(nearest_to_centroids)
   t.unload()
-
-  centroid_sentences = [
-    sentence_list[index] for array in nearest_to_centroids for index in array
+  centroid_sentence_groups = [
+    [sentence_list[index] for index in index_array]
+    for index_array in nearest_to_centroids
   ]
+  centroid_sentences = []
+  for group in centroid_sentence_groups:
+    filtered_group = [sent for sent in group
+      if sentence_is_good(sent, min_sent_words, max_sent_words)
+    ]
+    centroid_sentences.extend(filtered_group[:sentences_to_pick_per_cluster])
 
   summary_sentences = [
     sent.replace("\n", " ").replace("  ", " ").strip() for sent in centroid_sentences
@@ -80,3 +91,10 @@ def get_vector(sentence):
     return embedding.vector
   except:
     return
+
+def sentence_is_good(sent, min_sent_words, max_sent_words):
+  # Warning: word_tokenize will split a string like '6).' into three words.
+  words = nltk.word_tokenize(sent)
+  words = [word for word in words if real_word_regex.search(word)]
+  word_count = len(words)
+  return word_count >= min_sent_words and word_count <= max_sent_words
