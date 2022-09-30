2b403cb72380942cc75f03822b7bd6eabb7b8b5d
Jim Kang <jimkang@gmail.com>
2022-09-30 16:32:23 -0400

    Change test app default.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 A Flask app that routes http requests to summarize().
 """
 from flask import Flask, render_template, request, url_for, flash, redirect
-from summarize.summarize import summarize
+from .summarize.summarize import summarize
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2af224ae93084a1385c8e8cb4a6724448c548c19891cbb17'
